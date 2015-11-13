5955a69d141d3b4d475ebfa93fa546f7593c86d3
ValManP <valpozd@mail.ru>
2015-11-13 17:59:20 +0300

    game_stat at home.php

--- www/php/home.php
+++ www/php/home.php
@@ -22,20 +22,25 @@
 			<a href="game_start.php" class="link_button" style="margin-left: 40%;">Начать игру</a><br>
 			<div class="left">
 				<div class="game_stat" style = "width:110px; height:230px;">
-					<!-- Кнопки должны сабмитить в какую-то форму, поэтому нужно сделать хотя бы пустую: -->
 					<form id="stat_form" action="home.php" method="post"></form>
 					<?php while ($row = mysql_fetch_array ($result)): ?>
-						<!-- У кнопки нужно указать, к какой форме она привязана, а также тип submit, -->
-						<!-- так как тип button нужен для того, чтобы забить на кнопку JavaScript функцию: -->
-						<button type="submit" name="game" class="btn" form="stat_form" value="<?php echo $row['game_id']; ?>">Игра <?php echo $row['game_id']; ?></button>
+						<button type="submit" name="game" class="btn" form="stat_form" value="<?php echo $row['game_id']; ?>">Игра <?php echo $row['rank']; ?></button>
 					<?php endwhile; ?>
 				</div>
 			</div>
 			
 			<div class="right">
-				<div class="game_stat" style = "width:330px; height:230px; margin-left: 10%;">
-					<p><?php echo $stat; ?>:</p>
-				</div>
+				<form class="game_stat" action="home.php" style = "width:330px; height:230px; margin-left: 10%;">
+				<?php if (isset($game_history)){ ?>
+					<?php echo "Дата: ".$time; ?> <br/>
+					<?php echo "Задуманное число: ".$unknown_number; ?> <br/>
+					<?php if ($score != -1) echo "Угадано за ".$score." попыток."; else echo "Вы сдались =("; ?> <br/><br/>
+					<?php while ($row = mysql_fetch_array ($game_history)): ?>							
+							<?php echo "Ход ".substr($row['value'], 0, 4)." : "; ?>
+							<?php echo substr($row['value'], 4, 1); ?><img src="../source/bull.png" width="20px" height="25px"/>
+							<?php echo substr($row['value'], 5, 1); ?><img src="../source/cow.png" width="20px" height="23px"/><br/>
+					<?php endwhile;} ?>
+				</form>
 			</div>
 			
 		</div>
--- www/php/show_stat.php
+++ www/php/show_stat.php
@@ -4,15 +4,19 @@
 	if (!isset($result)) {
 		include("connect_db.php");
 		$user_id = $_SESSION['user_id'];
-		$result = mysql_query ("SELECT * FROM games_t WHERE user_id='$user_id'", $db); 
+		$result = mysql_query ("SELECT t . * , @rownum := @rownum +1 AS rank FROM games_t t, ( SELECT @rownum :=0)r WHERE t.user_id='$user_id' ORDER BY t.time", $db);
 	}
 	
 	if ($_SERVER["REQUEST_METHOD"] == "POST") {
-		echo "hello";
 		if (!empty($_POST['game']))
 		{
 			$game_id = $_POST['game'];
-			echo " from button " . $game_id;
+			$game_info = mysql_fetch_array (mysql_query ("SELECT * FROM games_t WHERE game_id='$game_id'", $db));
+			$unknown_number = $game_info['unknown_number'];
+			$time = $game_info['time'];
+			$score = $game_info['score'];
+
+			$game_history = mysql_query ("SELECT * FROM gametab_t WHERE game_id='$game_id'", $db);
 		}
 	}
 ?>
